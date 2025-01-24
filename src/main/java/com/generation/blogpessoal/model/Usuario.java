20722ff0d78f14d849d1b189f4707fe5f6f0c85b
Lucas300 <ludaniel.sd@gmail.com>
2025-01-24 16:16:40 -0300

    “Deploy”

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -29,6 +30,7 @@ import jakarta.validation.constraints.Size;
 		@NotNull(message = "O Atributo Nome é Obrigatório!")
 		private String nome;
 
+		@Schema(example = "email@email.com.br")
 		@NotNull(message = "O Atributo Usuário é Obrigatório!")
 		@Email(message = "O Atributo Usuário deve ser um email válido!")
 		private String usuario;
@@ -40,6 +42,8 @@ import jakarta.validation.constraints.Size;
 		@Size(max = 5000, message = "O link da foto não pode ser maior do que 5000 caracteres")
 		private String foto;
 
+		
+		
 		@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
 		@JsonIgnoreProperties("usuario")
 		private List<Postagem> postagem;
